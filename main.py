9cb9cf0b49e85c3a432a9262a14d8e7be4f1880c
Sushmita-Jacob <sushmita.cjacob@gmail.com>
2025-10-18 16:16:19 +0000

    Commit

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 #This program is the starter code for the Cipher Usability Project. 
 # This code is inspired by Cracking Codes with Python: An Introduction to Building and Breaking Ciphers by Al Sweigart https://www.nostarch.com/crackingcodes (BSD Licensed)
 
+import string
+
 # Global variables
-possibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 initialPosition = 0
 shiftedPosition = 0
 shiftedMessage = ""
+lettersLower = string.ascii_lowercase
+lettersUpper = string.ascii_uppercase
+numbers = string.digits
+symbols = string.punctuation
+possibleCharacters = lettersLower + lettersUpper + numbers + symbols
 
 # Define the function called encryptOrDecrypt
+def encryptOrDecrypt():
+  global shiftedPosition
+  if mode.lower() == "encrypt":
+    shiftedPosition = initialPosition + key
+  elif mode.lower() == "decrypt":
+    shiftedPosition = initialPosition - key
 
 # Define the function called wraparound
-
+def wraparound():
+  global shiftedPosition
+  if shiftedPosition >= len(possibleCharacters):
+    shiftedPosition = shiftedPosition - len(possibleCharacters)
+  elif shiftedPosition < 0:
+    shiftedPosition = shiftedPosition + len(possibleCharacters)
+    
 # Run code
 
 # Introduction
@@ -25,17 +43,9 @@ mode = input("Do you want to encrypt or decrypt? ")
 for character in initialMessage:
   if character in possibleCharacters:
     initialPosition = possibleCharacters.find(character)
-
-    if mode.lower() == "encrypt":
-      shiftedPosition = initialPosition + key
-    elif mode.lower() == "decrypt":
-      shiftedPosition = initialPosition - key
-
-    if shiftedPosition >= len(possibleCharacters):
-      shiftedPosition = shiftedPosition - len(possibleCharacters)
-    elif shiftedPosition < 0:
-      shiftedPosition = shiftedPosition + len(possibleCharacters)
-
+    encryptOrDecrypt()
+    wraparound()
+    
     shiftedMessage = shiftedMessage + possibleCharacters[shiftedPosition]
 
   else:
